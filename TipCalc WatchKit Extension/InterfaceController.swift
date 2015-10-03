e766e2f6fd5f0be916a2a2277291ca7bd9535ae2
Sheng Yu <shengyu@real.com>
2015-10-02 21:54:01 -0400

    Initial Commit

---
+++ TipCalc WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TipCalc WatchKit Extension
+//
+//  Created by Sheng Yu on 10/2/15.
+//  Copyright © 2015 Sheng Yu. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
